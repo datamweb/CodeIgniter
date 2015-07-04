ba480a9f2f0695baa8b994167f479222a8b0d40a
pooya parsa dadashi <pooya_parsa_dadashi@yahoo.com>
2015-07-05 03:48:05 +0430

    Create calendar_lang.php
    Add this file for Farsi language.
    This file is Farsi Translation for calendar_lang.php.
    To use must change the file: CodeIgniter \ application \ config.php
    $ config ['language'] = 'english';
    To
    $ config ['language'] = 'farsi';

---
+++ system/language/farsi/calendar_lang.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP
+ *
+ * This content is released under the MIT License (MIT)
+ *
+ * Copyright (c) 2014 - 2015, British Columbia Institute of Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @package	CodeIgniter
+ * @author	EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (http://ellislab.com/)
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ * @since	Version 1.0.0
+ * @filesource
+ */
+
+/*
+    Farsi (Persian) language for CodeIgniter 3.x system add by pooya parsa dadashi(datamweb)
+    git:: https://github.com/datamweb
+    web::www.datamweb.ir
+    info :: CodeIgniter Version 3.0.0 
+*/
+
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['cal_su'] = 'یکشنبه';
+$lang['cal_mo'] = 'دوشنبه';
+$lang['cal_tu'] = 'سه شنبه';
+$lang['cal_we'] = 'چهارشنبه';
+$lang['cal_th'] = 'پنچ شنبه';
+$lang['cal_fr'] = 'جمعه';
+$lang['cal_sa'] = 'شنبه';
+$lang['cal_sun'] = 'یکشنبه';
+$lang['cal_mon'] = 'دوشنبه';
+$lang['cal_tue'] = 'سه شنبه';
+$lang['cal_wed'] = 'چهارشنبه';
+$lang['cal_thu'] = 'پنچ شنبه';
+$lang['cal_fri'] = 'جمعه';
+$lang['cal_sat'] = 'شنبه';
+$lang['cal_sunday'] = 'یکشنبه';
+$lang['cal_monday'] = 'دوشنبه';
+$lang['cal_tuesday'] = 'سه شنبه';
+$lang['cal_wednesday'] = 'چهارشنبه';
+$lang['cal_thursday'] = 'پنچ شنبه';
+$lang['cal_friday'] = 'جمعه';
+$lang['cal_saturday'] = 'شنبه';
+$lang['cal_jan'] = 'ژانویه';
+$lang['cal_feb'] = 'فوریه';
+$lang['cal_mar'] = 'مارس';
+$lang['cal_apr'] = 'آوریل';
+$lang['cal_may'] = 'مه';
+$lang['cal_jun'] = 'ژوئن';
+$lang['cal_jul'] = 'جولای';
+$lang['cal_aug'] = 'اوت';
+$lang['cal_sep'] = 'سپتامبر';
+$lang['cal_oct'] = 'اکتبر';
+$lang['cal_nov'] = 'نوامبر';
+$lang['cal_dec'] = 'دسامبر';
+$lang['cal_january'] = 'ژانویه';
+$lang['cal_february'] = 'فوریه';
+$lang['cal_march'] = 'مارس';
+$lang['cal_april'] = 'آوریل';
+$lang['cal_mayl'] = 'مه';
+$lang['cal_june'] = 'ژوئن';
+$lang['cal_july'] = 'جولای';
+$lang['cal_august'] = 'اوت';
+$lang['cal_september'] = 'سپتامبر';
+$lang['cal_october'] = 'اکتبر';
+$lang['cal_november'] = 'نوامبر';
+$lang['cal_december'] = 'دسامبر';
